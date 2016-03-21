7eacd1f55337c1cfc7d5516a06ccf6dea39e7803
malof <malof@kth.se>
2016-03-21 12:46:20 +0100

    Initial commit for the agenda-view. Overall structure

--- app/js/agendaCtrl.js
+++ app/js/agendaCtrl.js
@@ -1,3 +1,23 @@
 agendaApp.controller('AgendaCtrl', function ($scope) {
 
+    $scope.types = [{
+        value: 'intro',
+        label: 'Intro'
+    }, {
+        value: 'outro',
+        label: 'Outro'
+    }];
+
+    $scope.confirmagenda ="Confirm agenda"
+    $scope.confirmediting ="Confirm editing"
+    $scope.invitedheadline ="Invited"
+    $scope.titlestring ="Title:"
+    $scope.durationstring ="Duration:"
+    $scope.categorystring ="Category:"
+    $scope.descriptionstring ="Description:"
+
+    $scope.inviting ="Enter user to invite"
+    $scope.meetingname ="Enter meeting name"
+    $scope.date ="Date"
+    $scope.starttime ="Starting Time"
 })
