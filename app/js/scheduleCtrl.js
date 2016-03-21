04dff198e3863d77b3eb38a1c3e3a5981cd8e043
Mark Wong <markwong@kth.se>
2016-03-21 16:09:30 +0100

    drag and drop påbörjat

--- app/js/scheduleCtrl.js
+++ app/js/scheduleCtrl.js
@@ -1,3 +1,27 @@
 agendaApp.controller('ScheduleCtrl', function ($scope) {
 
+function handleDragStart(e) {
+	this.style.opacity = '0.4';  // this / e.target is the source node.
+}
+
+function handleDragEnd(e) {
+	this.style.opacity = '1';
+}
+
+var delay=500; //1 seconds
+var cols = [];
+
+setTimeout(function(){
+	cols = document.querySelectorAll('.event');
+	console.log(cols);
+	[].forEach.call(cols, function(col) {
+  		col.addEventListener('dragstart', handleDragStart, false);
+		col.addEventListener('dragend', handleDragEnd, false);
+	});
+}, delay); 
+
+
+
+
+
 })
