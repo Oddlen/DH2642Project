e4852f6cae5bd6145c2478ef35f173fddcac5318
Oddlen <decylon@gmail.com>
2016-03-27 18:03:31 +0200

    Database and model starts
    Model that is still work in progress. A database "dumb" with database
    connection to the real database commented out at the top. And the
    security rules for the database.

---
+++ app/js/model.js
@@ -0,0 +1,132 @@
+// Example of function arguments.
+var exampleAgendaObject1 = {
+	name:"name1",
+	start:"08:25",
+	end:"09:00",
+	length:"0h35m",
+	catagoty:"Introduction"
+};
+
+var exampleAgendaObject2 = {
+	name:"name2",
+	start:"09:00",
+	end:"10:36",
+	length:"1h36m",
+	catagoty:"Other"
+};
+
+var exampleEventObject = {
+    day:"01",
+    month:"06",
+    year:"2016",
+    name:"OnlyLettersAndNumbers",
+    start:"08:25",
+    end:"10:36",
+    length:"2h11m",
+    invited:['username1', 'username2', 'username3'],
+    agenda:[exampleAgendaObject1, exampleAgendaObject2]
+};
+
+// callback function api.
+function exampleCallbackFunction(booleanFalseIfError, stringMessage) {
+	// body...
+}
+
+dataRef = new Firebase('https://dh2642.firebaseIO.com/');
+useRef = dataRef.child("users");
+eveRef = dataRef.child("events");
+catRef = dataRef.child("catagories");
+usernameRef = "";
+auth = null;
+
+function createUser(username, password, callbackFunction) {
+	dataRef.createUser({
+  			email: username + "@mail.com",
+  			password: password
+		}, 
+		function(error, userData) {
+  			if (error) {
+    			console.log("Error creating user:", error);
+    			callbackFunction(false, "Could not create the new user.");
+  			} else {
+    			console.log("Successfully created user account.");
+    			createUserStep2(username, password, callbackFunction);
+    			
+  			}
+		}
+	);
+}
+
+function createUserStep2(username, password, callbackFunction) {
+	var calbck = function createUserStep3(ok, message) {
+		if (ok){
+			useRef.update({
+  					[username]: {
+  						id: auth.uid,
+  						days: {}
+  					}
+  				}, function(error) {
+  					if (error) {
+    					console.log('Synchronization failed', error);
+    					callbackFunction(false, "Error when creating user.");
+  					} else {
+    					logout();
+  						callbackFunction(true, "User has been created.");
+  					}
+  				}
+  			);
+		} else {
+			callbackFunction(false, "Error when creating user.");
+		}
+	}
+	login(username, password, calbck);
+}
+
+function logout(){
+	dataRef.unauth();
+	usernameRed = "";
+	auth = null;
+}
+
+function login(username, password, callbackFunction) {
+	logout();
+	dataRef.authWithPassword({
+  			email: username + "@mail.com",
+  			password: password
+		}, function(error, authData) {
+  			if (error) {
+    			console.log("Login Failed!", error);
+   				callbackFunction(false, "Login Failed.");
+  			} else {
+    			console.log("Authenticated successfully.");
+    			usernameRef = username;
+    			auth = dataRef.getAuth();
+    			callbackFunction(true, "Authenticated successfully.");
+  			}
+		}
+	);
+}
+
+function getUID(username, callbackFunction) {
+	
+}
+
+function getDay(day, month, year, callbackFunction) {
+	// body...
+}
+
+function getEvent(day, month, year, eventName, callbackFunction) {
+	// body...
+}
+
+function setEvent(enventObject, callbackFunction) {
+	// body...
+}
+
+function getCatagories(callbackFunction) {
+	// body...
+}
+
+function invite(username, callbackFunction) {
+	// body...
+}
